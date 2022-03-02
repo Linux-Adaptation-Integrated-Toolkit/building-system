91e3f3ca5c195c7db4d6d9d744c7ea3a88f77929
zhaozhen <zhaozhen@uniontech.com>
2022-03-02 10:35:09 +0800

    update cmake logical code

--- cmake.cpp
+++ cmake.cpp
@@ -1,63 +1,71 @@
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
 #include <regex>
+#include <vector>
+
+#if __GNUC__ <= 5
+    #error your compiler cannot support C++17 standard. we need at least GCC 6
+#elif (__GNUC__ > 5) && (__GNUC__ < 11)
+    #include <experimental/filesystem>
+#else
+    #include <filesystem>
+#endif
+
+#include <fstream>
+#include <iostream>
+#include <string>
 
 using namespace std;
+#if (__GNUC__ > 6) && (__GNUC__ < 11)
+    namespace filesystem = experimental::filesystem;
+#endif
 
-struct lswap {
-  unsigned linenum;
-  string type; 
-};
+//remove space with cpp API
+string removeSpace(string str) {
+  str.erase(remove(str.begin(), str.end(), ' '), str.end());
+  //res.assign(res.begin(), res.end());
+  return str;
+}
+//remove space with regex
+string removeSpace2(string str) {
+  string p = "\x20";
+  regex express(p);
+  regex_replace(str, express, '\0');
+  return str;
+}
 
-/*
-  get the dependent list through substitude the required words to null,
-  after getting it, return the substitude back. 
-*/
-
-int main () {
-  string filepath = "./CMakeLists.txt";
-  vector<lswap> lines;
-  string line;
-  int linenum = 0;
-  string type;
-  lswap lineswap;
-
-  string p_required = "REQUIRED";
-  string p_find_package = "find_package";
-
-  ifstream input_file(filepath);
-  if (!input_file.is_open()) {
-      cerr << "Could not open the file - '"
-            << filepath << "'" << endl;
-      return -1;
+int main(int argc, char* argv[]) {
+  if(argc != 2) {
+    cerr << "please offer path of project directory (only)" << endl;
+    return 1;
   }
-
-  //replace the code
-  while(getline(input_file, line)) {
-    linenum++;
-    lineswap.linenum = linenum;
-    regex r(p_find_package);
-    bool regex_find_package = regex_search(line, r);
-    if (regex_find_package) {
-      lineswap.type = "optional";
-      regex p(p_required);
-      bool regex_required = regex_search(line, p);
-      if(regex_required) {
-        lineswap.type = "required";
-        regex_replace(line, p_required, "");
-      }
-      lines.push_back(lineswap);
-    }
+  filesystem::path CmakeRootPath = argv[1];
+  if(!filesystem::exists(CmakeRootPath)) {
+    cerr << "offer a correct path, and make sure it is a directory" << endl;
+    return 2;
   }
-  //make a new directory named build, then cd into execute cmake command
+  if(!filesystem::is_directory(CmakeRootPath)) {
+    cerr << "please offer a path of directory, not a file" << endl;
+    return 2;
+  }
+  string Cmakefile = (string)CmakeRootPath + (string)"/CMakeLists.txt";
+  if(!filesystem::exists(Cmakefile)) {
+    cerr << "the path you have offerd doesn't contain CMakeLists.txt in top directory." << endl;
+    return 2;
+  }
   
-  //recover the original code.
-  for (const auto &i : lines) {
-    cout << i.linenum << i.type << endl;
+  ifstream fin(Cmakefile, ios::in);
+  char line[1024] = {0};
+  while(fin.getline(line, sizeof(line))) {
+    string linestr = removeSpace(line);
+    if(linestr.empty()) {
+      continue;
+    }
+    if(linestr.at(0) == '#') {
+      continue;
+    }
+    string p = "find_package(";
   }
+  fin.clear();
+  fin.close();
 
-  input_file.close();
   return 0;
 }
